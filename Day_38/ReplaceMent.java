0475f8fa399cb2964d00efd103e87d782694e95e
CHSOQIWEN <44434467+CHSOQIWEN@users.noreply.github.com>
2019-07-16 22:05:28 +0800

    每日练习

---
+++ Day_38/ReplaceMent.java
@@ -0,0 +1,17 @@
+package com.bittch.Day_38;
+
+import java.util.Scanner;
+
+/**
+ * @author CHAOQIWEN
+ * @data 2019/7/16 19:15
+ */
+public class ReplaceMent {
+    public static String replaceSpace(String iniString, int length) {
+        // write code here
+        return iniString.replace(" ","%20");
+
+
+    }
+
+}
